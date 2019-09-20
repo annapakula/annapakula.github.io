88b3606496ca44aa59cc78c546cd97cc89f09550
Anna Mazurek <anna.mazurek.93@gmail.com>
2019-09-20 11:03:54 +0200

    Feat: skills section layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 "use strict";
 
-console.log(`Hello world!`);
+console.log(`Hello! I'm glad you're interested in my code. If you see something that I could do better, please contact me: anna.mazurek.93@gmail.com`);
 
 
