aa62702936e56ffa60044a213ac0042d06b5bd9e
Anna Mazurek <anna.mazurek.93@gmail.com>
2019-09-20 16:36:17 +0200

    Fix: git push & pull problems

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 "use strict";
 
-console.log(`Hello world!`);
+console.log(`Hello! 
+I'm glad you're interested in my code. If you see something I could do better, please write me: anna.mazurek.93@gmail.com`);
 
 
