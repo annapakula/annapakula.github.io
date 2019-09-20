ee7215a1336cb55a1913cc3cfccb01686fc6ad9f
Anna Mazurek <anna.mazurek.93@gmail.com>
2019-09-20 23:00:36 +0200

    Add fetch API

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,29 @@
 "use strict";
 
+const projectsList = document.querySelector(".projects__repos--js");
+
+fetch("https://api.github.com/users/annamazurek/repos")
+  .then(response => response.json())
+  .then(response => {
+    const repositories = response;
+    for(const repo of repositories){
+      projectsList.innerHTML += `<li class="projects__item"><a class="projects__link" href="${repo.html_url}">
+        ${repo.name} <br>
+        ${repo.description}
+      </a></li>`
+    }
+    console.log(response);
+  })
+  // .catch(error);
+  
+
+
+
+
+
+
+
+
+
 console.log(`Hello! 
 I'm glad you're interested in my code. If you see something I could do better, please write me: anna.mazurek.93@gmail.com`);
-
-
